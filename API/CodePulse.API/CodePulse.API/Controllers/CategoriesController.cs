b614678c1ec9ae577318056b3b145de68161b6c7
Lmdingi <lmdingi4@gmail.com>
2024-11-26 16:48:26 +0200

    update: api to create categoaries 	new file:   API/CodePulse.API/.vs/CodePulse.API/DesignTimeBuild/.dtbcache.v2 	new file:   API/CodePulse.API/.vs/CodePulse.API/FileContentIndex/06491a62-3878-4fb9-8cf5-851e474a83a2.vsidx 	new file:   API/CodePulse.API/.vs/CodePulse.API/FileContentIndex/19b39b24-e07b-4310-85d2-f3558c2249d4.vsidx 	new file:   API/CodePulse.API/.vs/CodePulse.API/FileContentIndex/4034916c-4416-45aa-b6fe-2758950f46fd.vsidx 	new file:   API/CodePulse.API/.vs/CodePulse.API/FileContentIndex/563d17ef-565c-48f1-83a2-71184c67cb1a.vsidx 	new file:   API/CodePulse.API/.vs/CodePulse.API/FileContentIndex/d815bafe-5c8f-4c5c-9878-faf415a4a1bd.vsidx 	new file:   API/CodePulse.API/.vs/CodePulse.API/config/applicationhost.config 	new file:   API/CodePulse.API/.vs/CodePulse.API/v17/.futdcache.v2 	new file:   API/CodePulse.API/.vs/CodePulse.API/v17/.suo 	new file:   API/CodePulse.API/.vs/CodePulse.API/v17/DocumentLayout.backup.json 	new file:   API/CodePulse.API/.vs/CodePulse.API/v17/DocumentLayout.json 	new file:   API/CodePulse.API/.vs/ProjectEvaluation/codepulse.api.metadata.v9.bin 	new file:   API/CodePulse.API/.vs/ProjectEvaluation/codepulse.api.projects.v9.bin 	new file:   API/CodePulse.API/.vs/ProjectEvaluation/codepulse.api.strings.v9.bin 	new file:   API/CodePulse.API/CodePulse.API.sln 	new file:   API/CodePulse.API/CodePulse.API/.gitignore 	new file:   API/CodePulse.API/CodePulse.API/CodePulse.API.csproj 	new file:   API/CodePulse.API/CodePulse.API/CodePulse.API.http 	new file:   API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs 	new file:   API/CodePulse.API/CodePulse.API/Controllers/WeatherForecastController.cs 	new file:   API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs 	new file:   API/CodePulse.API/CodePulse.API/Migrations/20241126130737_Initial Migration.Designer.cs 	new file:   API/CodePulse.API/CodePulse.API/Migrations/20241126130737_Initial Migration.cs 	new file:   API/CodePulse.API/CodePulse.API/Migrations/ApplicationDbContextModelSnapshot.cs 	new file:   API/CodePulse.API/CodePulse.API/Models/DTO/CategoryDto.cs 	new file:   API/CodePulse.API/CodePulse.API/Models/DTO/CreateCategoryRequest.cs 	new file:   API/CodePulse.API/CodePulse.API/Models/Domain/BlogPost.cs 	new file:   API/CodePulse.API/CodePulse.API/Models/Domain/Category.cs 	new file:   API/CodePulse.API/CodePulse.API/Program.cs 	new file:   API/CodePulse.API/CodePulse.API/Properties/launchSettings.json 	new file:   API/CodePulse.API/CodePulse.API/Repositories/Implementation/CategoryRepository.cs 	new file:   API/CodePulse.API/CodePulse.API/Repositories/Interface/ICategoryRepository.cs 	new file:   API/CodePulse.API/CodePulse.API/WeatherForecast.cs 	new file:   API/CodePulse.API/CodePulse.API/appsettings.Development.json 	new file:   API/CodePulse.API/CodePulse.API/appsettings.json

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequest request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+           await categoryRepository.CreateAsync(category);
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            return Ok(response);
+        }
+    }
+}
