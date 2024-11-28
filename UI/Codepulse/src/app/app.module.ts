fd08ec0b3632b2fef8ea4dc5b9d7e8a631bddb02
Lmdingi <lmdingi4@gmail.com>
2024-11-28 17:43:04 +0200

    update: UI add blog post

--- UI/Codepulse/src/app/app.module.ts
+++ UI/Codepulse/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
+import { MarkdownModule } from 'ngx-markdown';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,13 @@ import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blog
     BlogpostListComponent,
     AddBlogpostComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MarkdownModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
