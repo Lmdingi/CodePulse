7b1307277b66cd52bb98b5246a0f21dce01782ee
Lmdingi <lmdingi4@gmail.com>
2024-11-29 15:33:30 +0200

    update: UI edit blog post

--- UI/Codepulse/src/app/features/blog-post/services/blog-post.service.ts
+++ UI/Codepulse/src/app/features/blog-post/services/blog-post.service.ts
@@ -21,4 +21,8 @@ export class BlogPostService {
   getAllBlogPosts(): Observable<BlogPost[]> {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogpost`);
   }
+
+  getBlogPostById(id: string): Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogpost/${id}`);
+  }
 }
