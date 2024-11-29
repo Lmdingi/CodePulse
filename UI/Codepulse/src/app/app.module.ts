e3ce7ba8a6229302c718d6618055795a29792eb7
Lmdingi <lmdingi4@gmail.com>
2024-11-29 18:30:30 +0200

    update: UI image selector pop up

--- UI/Codepulse/src/app/app.module.ts
+++ UI/Codepulse/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpo
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
 import { MarkdownModule } from 'ngx-markdown';
 import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
+import { ImageSelectorComponent } from './shared/components/image-selector/image-selector.component';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-b
     BlogpostListComponent,
     AddBlogpostComponent,
     EditBlogpostComponent,
+    ImageSelectorComponent,
   ],
   imports: [
     BrowserModule,
