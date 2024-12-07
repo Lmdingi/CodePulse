606805b50c2d61d6dd713da07a3c1d299c8dade0
Lmdingi <lmdingi4@gmail.com>
2024-12-07 10:51:17 +0200

    update: UI auth guard

--- UI/Codepulse/src/app/features/auth/guards/auth.guard.ts
+++ UI/Codepulse/src/app/features/auth/guards/auth.guard.ts
@@ -1,5 +1,41 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../services/auth.service';
+import jwt_decode from 'jwt-decode';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const cookieService = inject(CookieService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getUser();
+
+  let token = cookieService.get('Authorization');
+
+  if (token && user) {
+    token = token.replace('Bearer', '');
+    const decodedToken: any = jwt_decode(token);
+
+    const expirationDate = decodedToken.exp * 1000;
+    const currentTime = new Date().getTime();
+
+    if (expirationDate < currentTime) {
+      authService.logout();
+      return router.createUrlTree(['/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+    } else {
+      if (user?.roles.includes('Writer')) {
+        return true;
+      } else {
+        alert('Unauthorized');
+        return false;
+      }
+    }
+  } else {
+    authService.logout();
+    return router.createUrlTree(['/login'], {
+      queryParams: { returnUrl: state.url },
+    });
+  }
 };
